65c5127731a15a09937c11bcd0df93a20ea234f4
Deepak's Project <deepak2rnath@yahoo.com>
2016-01-18 11:47:58 +0000

    Index Edit & Show

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,9 @@
 class EmployeesController < ApplicationController
     
+   def index
+      @employees=Employee.all 
+   end
+   
     def new
         
         @employee=Employee.new
@@ -12,7 +16,7 @@ class EmployeesController < ApplicationController
            
         if @employee.save
             flash[:notice]="The employee details are successfully added"
-            redirect_to Employee_path(@employee) 
+            redirect_to employee_path(@employee) 
         else
             render 'new'
            #redirect_to Employee_path(@employee)  
@@ -21,7 +25,26 @@ class EmployeesController < ApplicationController
     end
     
     def show
-     @employee = employee.find(params[:id])   
+     @employee = Employee.find(params[:id])   
+    end
+    
+    def edit
+       @employee = Employee.find(params[:id])   
+    end
+    
+    def update
+       @employee = Employee.find(params[:id]) 
+       
+       if  @employee.update(employee_params)
+           flash[:notice]="Employee details are successfully updated"
+           redirect_to employee_path(@employee)
+       else
+           render 'edit'
+       end
+           
+    end
+    
+    private
     def employee_params
         params.require(:employee).permit(:employee_name,:city,:mobile)
     end
